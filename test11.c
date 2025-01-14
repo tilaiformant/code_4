e7852b3aa469572c2716b228f15084b1952e2014
wujianyu <wujianyu322@gmail.com>
2025-01-14 14:27:31 +0800

    輸入整數值,輸出絕對值

--- test11.c
+++ test11.c
@@ -2,15 +2,13 @@
 
 int main()
 {
-    int age;
-    printf("請輸入年齡 : ");
-    scanf("%d", &age);
-    if (age < 20)
-    {
-        printf("你年齡太小\n");
-        printf("必年滿20歲才可以購買菸酒\n");
+    int num;
+    printf("請輸入整數值 : ");
+    scanf("%d", &num);
+    if (num < 0)
+        num = -num;
+        printf("絕對值是 %d\n", num);
     
-    }
     getchar();
     return 0;
 
