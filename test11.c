6ea3358bb4d3758b94ff936ffd9602a0d8c1a884
wujianyu <wujianyu322@gmail.com>
2025-01-15 12:05:12 +0800

    螢幕功能的選擇

--- test11.c
+++ test11.c
@@ -2,12 +2,24 @@
 
 int main()
 {
-    int a,b,c;
-
-    printf("請輸入任意 2 數字 ==> ");
-    scanf("%d%d", &a,&b);
-    c = ( a > b ) ? a : b;
-    printf("較大值是 %d \n",c);
+    int i;
+    printf("1. Access   .....\n");
+    printf("2. Excel    .....\n");
+    printf("3. Word     .....\n");
+    printf("請選擇 ==> ");
+    scanf("%d" ,&i);
+    printf("\n");
+    switch( i )
+    {
+        case 1: printf("Access 是資料庫軟體 \n");
+            break;
+        case 2: printf("Excel 是試算表軟體 \n");
+            break;
+        case 3: printf("Word 文書處理軟體 \n");
+            break;
+        default:
+            printf("選擇錯誤 \n");
+    }
     getchar();
     return 0;
 
