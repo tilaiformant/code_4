0d08f7e1686afd03a9de3debba1bf4e7362b810d
wujianyu <wujianyu322@gmail.com>
2025-01-16 12:17:54 +0800

    用switch,case編輯程式

--- test11.c
+++ test11.c
@@ -2,16 +2,40 @@
 
 int main()
 {
-    int i,j;
+    char grade;
 
-start:
-    printf("請輸入 2 個數字 \n==> ");
-    scanf("%d%d",&i,&j);
-    if( i > j ) /* 如果第 1 數字大於第 2 數字 */
-        goto stop; /* 跳至 stop 程式結束 */
-    goto start;
-stop:
-    printf("程式結束 \n");    
+    printf("請輸入成績等級(A,B,C,D,F) : ");
+    scanf("%c",&grade);
+    printf("\n");
+
+    switch ( grade )
+    {
+        case 'a':
+        case 'A':
+            printf("你是優等生 \n");
+            break;
+        case 'b':
+        case 'B':
+            printf("你是好等生 \n");
+            break;
+        case 'c':
+        case 'C':
+            printf("你是中等生 \n");
+            break;
+        case 'd':
+        case 'D':
+            printf("成績通過,但請加油 \n");
+            break;
+        case 'f':
+        case 'F':
+            printf("不及格 \n");
+            break;
+        default:
+            printf("請輸入正確的英文字 \n");
+        
+    }
+
+
     getchar();
     return 0;
 
